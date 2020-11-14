720547f54d9e5635edfcbe85c9ca22c62a96ebf1
mauriciosalichs <mauricio.salichs@gmail.com>
2020-11-14 16:35:50 -0300

    parser for options in command line

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,42 @@
 module Main where
 
 import System.Environment (getArgs)
+import Data.Semigroup ((<>))
+import Options.Applicative
 import Parse (fileParse)
 import Lang
 import Styles
 import BuildHTML (genHtml)
 
+
+data Opts = Opts
+    { numering :: Bool
+    , pageFormat :: String
+    , inputFile :: !String
+    , outputFile :: String
+    }
+
+optsParser :: ParserInfo Opts
+optsParser =
+    info
+        (helper <*> programOptions)
+        (fullDesc <> progDesc "Introduzca el nombre del archivo origen y el nombre del archivo salida" <>
+            header
+                "Generador de documentos HTML")
+
+programOptions :: Parser Opts
+programOptions =
+    Opts <$> switch (long "numeracion" <> short 'n' <> help "Ennumera las páginas") <*> 
+    strOption (long "formato" <> short 'f' <> value "a4" <> help "Formato de la página") <*>
+    strArgument (help "Archivo de origen") <*>
+    strArgument (value "test/output.html" <> help "Archivo salida")
+
 main :: IO ()
 main = do
+        opts <- execParser optsParser
         args <- getArgs
-        -- assert (length args == 2)        buscar alguna estructura monádica para esto
-        let input = head args
-        let output = last args
+        let input = inputFile opts
+        let output = outputFile opts
         x <- (readFile input)
         case fileParse x of
              Left e             -> do   print e
@@ -19,5 +44,6 @@ main = do
              Right (styles,doc) -> do   let styl = processStyle styles []
                                             html = genHtml doc styl
                                         writeFile output html
+                                        print (numering opts)
                                         return ()
         
