a7b39e272849170dd62af73d7e66aa6e5bdec838
mauriciosalichs <mauricio.salichs@gmail.com>
2020-12-02 21:23:57 -0300

    primera version que funciona con Blaze.Html (ignorando estilos)

--- src/BuildIntHTML.hs
+++ src/BuildIntHTML.hs
@@ -9,9 +9,6 @@ import Control.Monad.Reader
 
 type StyledHtml = Reader StyleDict' Html
 
-xx :: AttributeValue
-xx = "Hello World"
-
 createStyle :: Style -> String
 createStyle None = ""
 createStyle (St s f c a) = "font-size:" ++ s ++ "pt;"
@@ -46,23 +43,23 @@ tableRow [] = fromString ""
 tableRow (tr:trs) = do td $ tex tr
                        tableRow trs
                       
-tabl :: [TableRow] -> StyledHtml
-tabl [] = return (fromString "")
-tabl ((Tr n r):ts) = do addStyle n ((tr.tableRow) r)
+tabl :: [TableRow] -> Html
+tabl [] = fromString ""
+tabl ((Tr n r):ts) = do addStyle n (tr $ tableRow r)
                         tabl ts
 
-sectionBody :: [Int] -> SectionBody -> StyledHtml
+sectionBody :: [Int] -> SectionBody -> Html
 sectionBody _ (Paragraph n t) = addStyle n (p $ tex t)
 sectionBody _ (Items n i) = addStyle n (items i)
-sectionBody _ (Table n t) = do st <- tabl t
+sectionBody _ (Table n t) = do let st = tabl t
                                addStyle n (table st)
-sectionBody _ (Image n i) = return (img ! (src $ fromString i)) -- por ahora ignoramos el estilo
+sectionBody _ (Image n i) = img ! (src $ fromString i) -- por ahora ignoramos el estilo
 sectionBody n (Subsections s) = sections (1:n) s
 
 chapter :: [Int] -> String
 chapter c = foldr (++) " " (fmap (\x -> show x ++ ".") c)
 
-title :: [Int] -> Title -> StyledHtml
+title :: [Int] -> Title -> Html
 title n (T t s) = do let header = case length n + 1 of
                                     1 -> h1
                                     2 -> h2
@@ -70,30 +67,30 @@ title n (T t s) = do let header = case length n + 1 of
                                     4 -> h4
                                     5 -> h5
                                     _ -> h6
-                         tc = header $ do toHtml (chapter $ reverse n)
+                         tc = header $ do fromString (chapter $ reverse n)
                                           tex t
-                     addStyle s tc
+                     tc
                      
-sectionDef :: [Int] -> [SectionBody] -> StyledHtml
-sectionDef _ [] = return (fromString "")
+sectionDef :: [Int] -> [SectionBody] -> Html
+sectionDef _ [] = fromString ""
 sectionDef n (x:xs) = do sectionBody n x
                          sectionDef n xs
                             
-doSection :: [Int] -> Section -> StyledHtml
+doSection :: [Int] -> Section -> Html
 doSection n (S t sb) = do BuildIntHTML.title n t
                           sectionDef n sb
 
-sections :: [Int] -> [Section] -> StyledHtml
-sections _ [] = return (fromString "")
+sections :: [Int] -> [Section] -> Html
+sections _ [] = fromString ""
 sections n@(i:is) (x:xs) = do doSection n x
                               sections ((i+1):is) xs
 
-build :: Document -> StyledHtml
+build :: Document -> Html
 build (D t ss) = do BuildIntHTML.title [] t
                     sections [1] ss
          
 genHtml :: Document -> StyleDict' -> Html
-genHtml s d = docTypeHtml ((runReader (build s)) d)
+genHtml s d = docTypeHtml $ body (build s)
          
 --genHtml :: Document -> StyleDict' -> String
 --genHtml s d = renderHtml $ body ((runReader (build s)) d)
